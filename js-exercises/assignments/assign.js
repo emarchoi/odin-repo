75047dbaa11107f9730eec28ab9e344dd6ba0ec7
choi <reymarpgg@gmail.com>
2026-07-10 12:21:35 +0800

    finishing

--- js-exercises/assignments/assign.js
+++ js-exercises/assignments/assign.js
@@ -90,7 +90,98 @@ function unique(arr) {
 
     return un;
 }
-// let strings = ["Hare", "Krishna", "Hare", "Krishna",
-//     "Krishna", "Krishna", "Hare", "Hare", ":-O"
-// ];
-// console.log(unique(strings));
+
+// 07. Map to Names
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = users.map(item => ` ${item.name}`);
+
+// alert( names  );
+
+
+// 08. Map to Objects
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = users.map(item => ({
+//     fullName: `${item.name} ${item.surname}`,
+//     id: item.id
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+
+
+// 09. Sort User by Age
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+// function sortByAge(arr){
+//     arr.sort((a, b) => a.age - b.age );
+// }
+
+
+// 010. Get Average Age
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+// function getAverageAge(arr){
+//     return arr.reduce((prev, user) => prev + user.age, 0) / arr.length;
+// }
+
+
+// 011. Create Keyed Object from Array
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+  
+  let usersById = groupById(users);
+  
+  /*
+  // after the call we should have:
+  
+  usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+  }
+  */
+
+  function groupById(users){
+    return users.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    }, {})
+  }
